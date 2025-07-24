02c43607b2e07fe9bb27fa630c41c58f2b85f37b
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-07-24 11:52:04 -0600

    Se va acompletando el formulario poco a poco, dandole interactividad y metodos que verifican que los datos sean correctos

--- src/app/shared/components/register-page/register-page.component.ts
+++ src/app/shared/components/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl} from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -14,16 +14,30 @@ export class RegisterPageComponent {
   formUtils = FormUtils;
 
   myForm: FormGroup = this.fb.group({
-    name: ["", Validators.required],
-    email: ["", [Validators.required ,Validators.email]],
-    username: ["", [Validators.required, Validators.minLength(6)]],
+    name: ["", [Validators.required, Validators.pattern( FormUtils.namePattern )]],
+    email: ["", [Validators.required ,Validators.pattern( FormUtils.emailPattern )]],
+    username: ["", [Validators.required, Validators.minLength(6), Validators.pattern( FormUtils.notOnlySpacesPattern)]],
+
     password: ["", [Validators.required, Validators.minLength(6)]],
     confirmPassword: ["", Validators.required ],
+  }, {
+    validators: [
+      this.isFieldOneEqualFieldTwo("password", "confirmPassword"),
+    ]
   });
 
+  isFieldOneEqualFieldTwo( field1: string, field2: string){
+    return ( formGroup: AbstractControl ) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordNotEqual: true };
+    };
+  }
 
   onSubmit() {
-    throw new Error('Method not implemented.');
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
   }
 
 }
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -3,6 +3,10 @@ import { FormArray, FormControl, FormGroup, ValidationErrors } from "@angular/fo
 
 export class FormUtils {
 
+  //EXPRESIONES REGULARES
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextError(errors: ValidationErrors){
       for(const key of Object.keys(errors)){
@@ -11,10 +15,23 @@ export class FormUtils {
           return 'Este campo es requerido';
 
           case 'minlength':
-            return `Minimo de ${errors['minlength'].requiredLength} caracteres.`
+            return `Minimo de ${errors['minlength'].requiredLength} caracteres.`;
 
           case 'min':
-            return `Valor minimo de ${ errors['min'].min}.`
+            return `Valor minimo de ${ errors['min'].min}.`;
+
+          case 'email':
+            return `El valor ingresado no es un correo electronico`;
+
+          case 'pattern':
+            if( errors['pattern'].requiredPattern === FormUtils.emailPattern ){
+              return `El correo electronico no es permitido`
+            }
+
+            return 'Error de patron de expresion regular';
+
+          default:
+            return `Error de validacion no controlado ${key}`;
     }
   }
   return null;
